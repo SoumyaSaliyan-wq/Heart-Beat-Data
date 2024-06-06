94df7ae1570267d813f8707173a112eab540a594
soumya <soumya@bit01.tech>
2024-06-06 11:18:04 +0530

    updated gitignore

--- app/modules/heartrate/heartrate.controller.js
+++ app/modules/heartrate/heartrate.controller.js
@@ -1,31 +1,31 @@
-const heartRateHelper=require('./heartrate.helper')
+const heartRateHelper = require('./heartrate.helper')
 const fs = require('fs');
-const responseHelper=require('../../helpers/response.helper');
-const responseMessageHelper=require('../../helpers/response_message.helper')
+const responseHelper = require('../../helpers/response.helper');
+const responseMessageHelper = require('../../helpers/response_message.helper')
 const logger = require('../../utils/logger');
 const batchSize = 100;
 const path = require('path');
-const hearRateService=require('../heartrate/heartrate.service');
+const hearRateService = require('../heartrate/heartrate.service');
 
 module.exports.processHeartRateData = (req, res) => {
     try {
         if (!req.file)
             return responseHelper.badRequestError(res, 'Please select a file to upload')
-        let filePath=path.join(process.cwd(), 'app','data','data.json')
-        fs.readFile(filePath, 'utf8',async (err, data) => {
-            if (err) {
-                return responseHelper.badRequestError(res, responseMessageHelper.heartBeat.ERROR)
-            }
-            const jsonData = JSON.parse(data);
-            const processedData = heartRateHelper.processBatch(jsonData, batchSize);
-            let findResult = await hearRateService.findData({ patient_id: jsonData.patient_id })
-            if (findResult) {
-                await hearRateService.updateData({ data: processedData }, { patient_id: jsonData.patient_id })
+        let filePath = path.join(process.cwd(), 'app', 'data', 'data.json')
+        fs.readFile(filePath, 'utf8', async (err, data) => {
+        if (err) {
+            return responseHelper.badRequestError(res, responseMessageHelper.heartBeat.ERROR)
+        }
+        const jsonData = JSON.parse(data);
+        const processedData = heartRateHelper.processBatch(jsonData, batchSize);
+        let findResult = await hearRateService.findData({ patient_id: jsonData.patient_id })
+        if (findResult) {
+            await hearRateService.updateData({ data: processedData }, { patient_id: jsonData.patient_id })
 
-            } else {
-                await hearRateService.createData({ data: processedData, patient_id: jsonData.patient_id })
-            }
-            return responseHelper.success(res, responseMessageHelper.heartBeat.SUCCESS, processedData)
+        } else {
+            await hearRateService.createData({ data: processedData, patient_id: jsonData.patient_id })
+        }
+        return responseHelper.success(res, responseMessageHelper.heartBeat.SUCCESS, processedData)
         })
     }
     catch (error) {
